af7bf11c25ebd2ba24ff1b8ac54507a79e06413d
Sergei Bozhko <sbozhko@mpi-sws.org>
2024-01-02 22:57:12 +0100

    tweak [distance] for better user experience

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,19 @@ interface Coordinate {
   y: number;
 }
 
-function distance(p1: Coordinate, p2: Coordinate): number {
+function skewedDistance(p1: Coordinate, p2: Coordinate): number {
+  // Controls the effect of each component on the total distance. When
+  // one drags a word, this function calculates the distance to the
+  // closest non-dragged word. If there are multiple rows, we would
+  // like the dragged word to favor words on the same row over words
+  // on other rows. The Y-axis has a higher multiplier to force such
+  // behavior.
+  const MULT_X = 1;
+  const MULT_Y = 10;
+
   const dx = p1.x - p2.x;
   const dy = p1.y - p2.y;
-  return Math.sqrt(dx * dx + dy * dy);
+  return Math.sqrt(MULT_X * dx * dx + MULT_Y * dy * dy);
 }
 
 function findClosestIndex(coord: Coordinate[], p: Coordinate): number | null {
@@ -18,10 +27,10 @@ function findClosestIndex(coord: Coordinate[], p: Coordinate): number | null {
   }
 
   let closestIndex = 0;
-  let closestDistance = distance(coord[0], p);
+  let closestDistance = skewedDistance(coord[0], p);
 
   for (let i = 1; i < coord.length; i++) {
-    const currentDistance = distance(coord[i], p);
+    const currentDistance = skewedDistance(coord[i], p);
     if (currentDistance < closestDistance) {
       closestIndex = i;
       closestDistance = currentDistance;
