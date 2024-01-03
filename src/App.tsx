c5854c772352c1fdf3da96460bbeedb9399a07e6
Sergei Bozhko <sbozhko@mpi-sws.org>
2024-01-03 16:25:14 +0100

    tweak a few things here and there

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,22 @@ interface Coordinate {
   y: number;
 }
 
-function skewedDistance(p1: Coordinate, p2: Coordinate): number {
-  // Controls the effect of each component on the total distance. When
-  // one drags a word, this function calculates the distance to the
-  // closest non-dragged word. If there are multiple rows, we would
-  // like the dragged word to favor words on the same row over words
-  // on other rows. The Y-axis has a higher multiplier to force such
-  // behavior.
-  const MULT_X = 1;
-  const MULT_Y = 10;
-
-  const dx = p1.x - p2.x;
-  const dy = p1.y - p2.y;
-  return Math.sqrt(MULT_X * dx * dx + MULT_Y * dy * dy);
-}
-
 function findClosestIndex(coord: Coordinate[], p: Coordinate): number | null {
+  function skewedDistance(p1: Coordinate, p2: Coordinate): number {
+    // Controls the effect of each component on the total distance. When
+    // one drags a word, this function calculates the distance to the
+    // closest non-dragged word. If there are multiple rows, we would
+    // like the dragged word to favor words on the same row over words
+    // on other rows. The Y-axis has a higher multiplier to force such
+    // behavior.
+    const MULT_X = 1;
+    const MULT_Y = 10;
+
+    const dx = p1.x - p2.x;
+    const dy = p1.y - p2.y;
+    return Math.sqrt(MULT_X * dx * dx + MULT_Y * dy * dy);
+  }
+
   if (coord.length === 0) {
     return null;
   }
@@ -263,10 +263,8 @@ function App() {
 
   return (
     <div className="h-full w-full max-w-xl flex-col pt-10 font-mono">
-      <div className="m-2 flex justify-center rounded-2xl border bg-white shadow-xl">
-        <div className="p-10 text-xl font-medium text-blue-900">
-          {inputSentence}
-        </div>
+      <div className="m-2 flex justify-center rounded-2xl border bg-white p-10 text-xl font-medium text-blue-900 shadow-xl">
+        {inputSentence}
       </div>
       <div className="m-2 flex justify-center rounded-2xl border bg-white p-8 shadow-xl">
         <Puzzle
