18787f5123348e9f515ad9a1c6ea65ddd3d6f392
Sergei Bozhko <sbozhko@mpi-sws.org>
2024-01-21 00:28:09 +0100

    add possibility to change avatar

--- client/src/components/profile.tsx
+++ client/src/components/profile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,11 +7,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Cookies from "js-cookie";
 import Avatar, { genConfig } from "react-nice-avatar";
 import { generateCookie } from "@/util/cookie";
 
 export default function Profile() {
-  const config = genConfig(generateCookie("visitor_id"));
+  const [config, setConfig] = useState(
+    genConfig(generateCookie("visitor_avatar")),
+  );
+
+  const changeAvatar = () => {
+    Cookies.remove("visitor_avatar");
+    setConfig(genConfig(generateCookie("visitor_avatar")));
+  };
 
   return (
     <DropdownMenu>
@@ -21,6 +29,9 @@ export default function Profile() {
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={changeAvatar}>
+          Change avatar
+        </DropdownMenuItem>
         <DropdownMenuItem disabled className="text-gray-300">
           Profile
         </DropdownMenuItem>
